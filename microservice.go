3ea6fe972d4aaf01f04f46886025ed5f31a5cf54
Pedro Gallego <pedro.delgallego@gmail.com>
2017-07-16 13:01:29 +0200

    all books endpoint

--- microservice.go
+++ microservice.go
@@ -1,33 +1,37 @@
 package main
 
-import "net/http"
-import "os"
-import "fmt"
+import (
+	"cloud-native-go/api"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-    http.HandleFunc("/", index)
-    http.HandleFunc("/echo", echo)
-    http.ListenAndServe(port(), nil)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/books", api.BooksHandleFunc)
+	http.ListenAndServe(port(), nil)
 }
 
 func port() string {
-    port := os.Getenv("port")
+	port := os.Getenv("port")
 
-    if len(port) == 0 {
-        port = ":8080"
-    }
+	if len(port) == 0 {
+		port = ":8080"
+	}
 
-    return port
+	return port
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Hello cloud native go")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello cloud native go")
 }
 
 func echo(w http.ResponseWriter, r *http.Request) {
-    message := r.URL.Query()["message"][0]
-    w.WriteHeader(http.StatusOK)
-    w.Header().Add("Content-Type", "text/plain")
-    fmt.Fprintf(w, message)
+	message := r.URL.Query()["message"][0]
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "text/plain")
+	fmt.Fprintf(w, message)
 }
