71021bd432f233ff19e6dd23cbf7b7ac39141554
Pedro Gallego <pedro.delgallego@gmail.com>
2017-07-16 03:05:51 +0200

    echo endpoint

---
+++ microservice.go
@@ -0,0 +1,33 @@
+package main
+
+import "net/http"
+import "os"
+import "fmt"
+
+func main() {
+    http.HandleFunc("/", index)
+    http.HandleFunc("/echo", echo)
+    http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+    port := os.Getenv("port")
+
+    if len(port) == 0 {
+        port = ":8080"
+    }
+
+    return port
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "Hello cloud native go")
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+    message := r.URL.Query()["message"][0]
+    w.WriteHeader(http.StatusOK)
+    w.Header().Add("Content-Type", "text/plain")
+    fmt.Fprintf(w, message)
+}
